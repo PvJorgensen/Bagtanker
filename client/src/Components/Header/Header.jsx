491676f317314f446101d30bf45fed25adadccd9
Patrik_Jørgensen <vadmand806@gmail.com>
2024-08-22 18:41:47 +0200

    Header styling justeringer

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styles from './header.module.scss'
 import { BurgerMenu } from '../BurgerMenu/BurgerMenu'
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <header>
+      <NavLink to='/'>
       <h1>Bagtanker</h1>
       <div className={styles.circle}></div>
+      </NavLink>
       <BurgerMenu></BurgerMenu>
     </header>
   )
--- client/src/Components/SecondHeader/SecondHeader.jsx
+++ client/src/Components/SecondHeader/SecondHeader.jsx
@@ -1,11 +1,14 @@
+import { NavLink } from 'react-router-dom'
 import { BurgerMenu } from '../BurgerMenu/BurgerMenu'
 import styles from './secondheader.module.scss'
 
 export const SecondHeader = () => {
   return (
     <header className={styles.scndHeader}>
+      <NavLink to='/'>
       <h1>Bagtanker</h1>
       <div className={styles.circle}></div>
+      </NavLink>
       <BurgerMenu />
     </header>
   )
